d9d745e74a92085854282f14c8242c5568ab8e55
prem kumar <freeazure001@gmail.com>
2021-10-06 17:25:18 +0530

    dotnet

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+
+    public class StudentController : Controller
+    {
+
+        // GET: Student
+        public ActionResult Create()
+        {
+            //fetch students from the DB using Entity Framework here
+
+            return View();
+        }
+    }
+
+
+}
